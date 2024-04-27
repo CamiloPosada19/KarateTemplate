b84a896dee9ed105584cd3015d0675c2d6a9ce9f
Camilo <90696295+CamiloPosada19@users.noreply.github.com>
2024-04-27 21:24:35 +0200

    Feat: Added cucumber reporters

--- myproject/src/test/java/features/ManagementTest.java
+++ myproject/src/test/java/features/ManagementTest.java
@@ -4,15 +4,38 @@ import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 class ManagementTest {
 
-    @Test
+    
+   @Test
     void testParallel() {
         Results results = Runner.path("classpath:features")
-                //.outputCucumberJson(true)
+                .outputCucumberJson(true)
+                .karateEnv("demo")
                 .parallel(5);
-        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+        generateReport(results.getReportDir());
+        assertTrue(results.getFailCount() == 0, results.getErrorMessages());
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList<>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "demo");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
     }
 
 }
